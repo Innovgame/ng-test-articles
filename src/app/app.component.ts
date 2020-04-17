b544172e65f7535fe7c2c25a6a9a6773844a299e
lc <innovgame@163.com>
2020-04-17 22:20:17 +0800

    ng 模版变量和事件函数绑定

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-hello-world';
+
+  addArticle(newTitle: HTMLInputElement, newLink: HTMLInputElement): boolean {
+    console.log(`${newTitle.value} + ${newLink.value}`);
+
+    return false;
+  }
 }
