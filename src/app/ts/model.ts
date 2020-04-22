f6717bbbb1a1dd1b8787963848870e9694365291
lc <innovgame@163.com>
2020-04-23 00:00:20 +0800

    add model ts

--- src/app/ts/model.ts
+++ src/app/ts/model.ts
@@ -7,3 +7,41 @@ export class User {
     this.id = uuid();
   }
 }
+
+export class Thread {
+  id: string;
+  lastMessage: Message;
+  name: string;
+  avatarSrc: string;
+
+  constructor(id?: string, name?: string, avatarSrc?: string) {
+    this.id = id;
+    this.name = name;
+    this.avatarSrc = avatarSrc;
+  }
+}
+
+export class Message {
+  id: string;
+  sendAt: Date;
+  isRead: boolean;
+  author: User;
+  text: string;
+  thread: Thread;
+
+  constructor(obj?: {
+    id?: string;
+    sendAt?: Date;
+    isRead?: boolean;
+    author?: User;
+    text?: string;
+    thread?: Thread;
+  }) {
+    this.id = (obj && obj.id) || uuid();
+    this.isRead = (obj && obj.isRead) || false;
+    this.sendAt = (obj && obj.sendAt) || new Date();
+    this.author = (obj && obj.author) || null;
+    this.text = (obj && obj.text) || null;
+    this.thread = (obj && obj.thread) || null;
+  }
+}
